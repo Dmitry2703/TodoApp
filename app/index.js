451b15f79a7ecfd04263a9e267974b6a345e2a25
Dmitry2703 <dmitri.meshcheryakov@gmail.com>
2017-02-03 08:54:17 +0600

    Add redux devtools

--- app/index.js
+++ app/index.js
@@ -5,12 +5,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, compose } from 'redux';
 import TodoApp from './components/TodoApp';
 import todoApp from './reducers/todoApp';
 
+const enhancers = compose(window.devToolsExtension ? window.devToolsExtension() : f => f);
+
+const store = createStore(todoApp, enhancers);
+
 ReactDOM.render(
-  <Provider store={createStore(todoApp)}>
+  <Provider store={store}>
     <TodoApp />
   </Provider>,
   document.getElementById('app')
